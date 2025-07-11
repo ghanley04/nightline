2973d8d998ae157da2fc0fe2479c61d2b231c887
Gillian Hanley <gghanley04@gmail.com>
2025-07-10 21:10:41 -0500

    added module

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,16 @@ module.exports = function(api) {
     api.cache(true); // You can keep this
     return {
       presets: ['babel-preset-expo'],
+      plugins: [
+        [
+          'module-resolver',
+          {
+            // This is the crucial part: maps @/ to your project root
+            alias: {
+              '@': './',
+            },
+          },
+        ],
+      ],
     };
   };
